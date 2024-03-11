562b613542a58082f5238e8ceda4057a6d9b04a4
Jiří <kingofmatrix.live@gmail.com>
2024-03-11 19:54:53 +0100

    du

--- src/main/java/cz/krokviak/donkeykong/main/Game.java
+++ src/main/java/cz/krokviak/donkeykong/main/Game.java
@@ -6,25 +6,36 @@ import javafx.animation.AnimationTimer;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.Pane;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Game {
+    private static final Logger LOGGER = LogManager.getLogger(Game.class);
     private final AnimationTimer gameLoop;
     private final GameStateManager gsm;
     private long lastUpdate = 0;
     public Game(final Pane pane,
                 final InputHandler inputHandler) {
+        LOGGER.info("Creating game");
         gameLoop = new AnimationTimer() {
             @Override
             public void handle(long now) {
                 if (lastUpdate > 0) {
+                    LOGGER.debug("Updating game");
                     final float dt = (now - lastUpdate) / 1_000_000_000f;
                     update(dt);
                 }
                 lastUpdate = now;
+                LOGGER.debug("Drawing game");
                 draw();
             }
         };
-        gsm = new GameStateManager(inputHandler, pane);
+        try {
+            gsm = new GameStateManager(inputHandler, pane);
+        } catch (Exception e){
+            LOGGER.error("Error creating game", e);
+            throw e;
+        }
     }
 
     public void start() {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,17 +1,8 @@
 module cz.krokviak.donkeykong {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.apache.logging.log4j;
     opens cz.krokviak.donkeykong to javafx.fxml;
+    opens cz.krokviak.donkeykong.test to javafx.fxml;
     exports cz.krokviak.donkeykong.main;
-    exports cz.krokviak.donkeykong.gamestate;
-    exports cz.krokviak.donkeykong.input;
-    exports cz.krokviak.donkeykong.drawable;
-    exports cz.krokviak.donkeykong.objects;
-    opens cz.krokviak.donkeykong.main to javafx.fxml;
-    exports cz.krokviak.donkeykong.items;
-    exports cz.krokviak.donkeykong.objects.barrels;
-    exports cz.krokviak.donkeykong.objects.ladder;
-    exports cz.krokviak.donkeykong.objects.climb;
-    exports cz.krokviak.donkeykong.objects.player;
-    exports cz.krokviak.donkeykong.objects.princess;
 }
